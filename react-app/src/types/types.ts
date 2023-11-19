3cee1c8dd0f46a6cc03d2234463e34234e174be0
OshkarVTec <a01275287@tec.mx>
2023-11-19 12:00:01 -0600

    update event type

--- react-app/src/types/types.ts
+++ react-app/src/types/types.ts
@@ -1,6 +1,8 @@
 export interface event{
     title: string,
     date: string,
+    time: string,
+    place: string,
     description: string,
     groupName: string,
     authorizedToEdit: boolean,
