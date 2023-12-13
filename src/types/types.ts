5624ab11db68d7b20c78ce5c4e584fa4f8c2200d
OshkarVTec <a01275287@tec.mx>
2023-12-13 17:41:33 -0600

    add user

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,19 +1,26 @@
-export interface event{
-    id: bigint,
-    title: string,
-    date: string,
-    time: string,
-    place: string,
-    description: string,
-    groupName: string,
-    authorizedToEdit: boolean,
+export interface event {
+	id: bigint;
+	title: string;
+	date: string;
+	time: string;
+	place: string;
+	description: string;
+	groupName: string;
+	authorizedToEdit: boolean;
 }
 
-export interface blogPost{
-    id: bigint,
-    title: string,
-    date: string,
-    text: string,
-    image: string,
-    authorizedToEdit: boolean,
+export interface blogPost {
+	id: bigint;
+	title: string;
+	date: string;
+	text: string;
+	image: string;
+	authorizedToEdit: boolean;
+}
+
+export interface user {
+	id: number;
+	nombre: string;
+	correo: string;
+	rol: string;
 }
