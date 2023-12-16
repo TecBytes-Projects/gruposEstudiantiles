16700cba5d10f0b5c24a09449980f1a948802d36
OshkarVTec <a01275287@tec.mx>
2023-12-16 17:30:23 -0600

    add hook to retreive on single object

--- src/customHooks/api.tsx
+++ src/customHooks/api.tsx
@@ -25,3 +25,24 @@ export function useFetch<Type>(link: string, token: string | null) {
 
 	return data;
 }
+
+export function useFetchDetails<Type>(link: string, token: string | null) {
+	const [data, setData] = useState<Type>();
+
+	useEffect(() => {
+		axios
+			.get(link, {
+				headers: {
+					Authorization: "Bearer " + token,
+				},
+			})
+			.then(function (response: AxiosResponse) {
+				setData(response.data);
+			})
+			.catch(function (error: AxiosError) {
+				console.log(error);
+			});
+	}, []);
+
+	return data;
+}
